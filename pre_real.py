fa9505a8d1af47df24bdb5604fe25d9296537f89
jayarnim <116495744+jayarnim@users.noreply.github.com>
2022-11-10 02:02:48 +0900

    Update pre_real.py

--- pre_real.py
+++ pre_real.py
@@ -1,15 +1,18 @@
-# 메인함수 : pre_real(df, real_col)
-# 기능 :
-    # 1/ 실질변수의 이상치를 상한값 및 하한값으로 치환함; Tukey Fences 기법 적용
-    # 2/ 실질변수를 표준화(Stanard)함; 
-    # 3/ 실질변수를 정규화(MinMax)함; 변수 범위의 상대적 크기가 주는 영향력을 최소화하기 위함
-# 반환값 : 실질변수가 전처리된 df
-# 매개변수 :
+# 메인함수 :
+    # pre_real(df, real_col)
     # df : 함수를 적용할 데이터프레임
     # real_col : 전처리할 실질변수 칼럼명이 기입된 리스트
 
+# 기능 :
+    # 데이터프레임 df의 실질변수 이상치를 상한값 및 하한값으로 치환함; Tukey Fences 기법 적용
+    # 데이터프레임 df의 실질변수를 표준화함; StanardScaler 적용
+    # 데이터프레임 df의 실질변수를 정규화함; MinMaxScaler 적용
+
+# 반환값 :
+    # 실질변수가 전처리된 df
     
     
+
 import numpy as np
 import pandas as pd
 

c03b8c760034ffde42a7933934d7fbf5bfa75595
jayarnim <116495744+jayarnim@users.noreply.github.com>
2022-11-09 20:37:26 +0900

    Update pre_nom.py

--- pre_nom.py
+++ pre_nom.py
@@ -1,3 +1,15 @@
+# 메인함수 : pre_nom(df, label_col = False, onehot_col = False)
+# 기능 :
+    # 1/ 명목변수를 Label Encoding함
+    # 2/ 명목변수를 One-Hot Encoding함
+# 반환값 : 명목변수가 전처리된 df
+# 매개변수 :
+    # df : 함수를 적용할 데이터프레임
+    # label_col : Label Encoding할 명목변수 칼럼명이 기입된 리스트 (Label Encoding할 칼럼이 없으면 생략함)
+    # onehot_col : One-Hot Encoding할 명목변수 칼럼명이 기입된 리스트 (One-Hot Encoding할 칼럼이 없으면 생략함)
+
+    
+
 import numpy as np
 import pandas as pd
 
